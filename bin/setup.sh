30efa64416acc6aaf614ade1ed81cc5e75d9b8e6
leileimiao <zhanghonglei1997@163.com>
2019-08-09 19:42:34 +0800

    update

--- bin/setup.sh
+++ bin/setup.sh
@@ -185,7 +185,7 @@ autolab_setup() {
 [mysqld]
 sql_mode='STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'
 EOF
-    services mysql restart
+    service mysql restart
     cd $AUTOLAB_PATH
     USE='yuzu'
     log "Initializing Autolab configurations..."
